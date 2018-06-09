a71fe72d9bf4d68563bca783793680bf3690ce52
Angithagbm <angithagbm@gmail.com>
2018-06-09 12:29:16 +0530

    added untracked file

---
+++ LearnsersIndia/Model/LIVideoModel.swift
@@ -0,0 +1,13 @@
+//
+//  LIVideoModel.swift
+//  LearnsersIndia
+//
+//  Created by GB Mainframe LLP on 09/06/18.
+//  Copyright © 2018 macbook. All rights reserved.
+//
+
+import UIKit
+
+class LIVideoModel: NSObject {
+
+}
